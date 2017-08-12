8f536cd876978667c802f558cdbb0a4fc1b27cbf
joarl940 <30956248+joarl940@users.noreply.github.com>
2017-08-12 12:22:19 -0500

    Update EjercicioG2.java

--- src/ejerciciog2/EjercicioG2.java
+++ src/ejerciciog2/EjercicioG2.java
@@ -16,7 +16,7 @@ public class EjercicioG2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("hola mundo desde git");
+        System.out.print("hola mundo desde git hola");
     }
     
 }
